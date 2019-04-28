751e4787d36d9eaaa797ee1f8606a8c88a2ee8eb
Mike Kennedy <mikekennedy75234@gmail.com>
2019-04-28 11:04:07 -0500

    url routing in place, blog app is done(working from admin only)

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('<int:blog_id>', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 
 def landing(request):
-    return render(request, 'blog/landing.html')
+    blogs = Blog.objects.all()
+    return render(request, 'blog/landing.html', {'blogs': blogs})
+
+
+def detail(request,blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})    
